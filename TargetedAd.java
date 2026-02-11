987620e4de943bf5192fd338d2fb00e89408ae53
Avinash Amanchi <148393523+avinashamanchi@users.noreply.github.com>
2026-02-11 11:11:14 -0800

    Implement targeted advertisement logic in TargetedAd

--- TargetedAd.java
+++ TargetedAd.java
@@ -5,36 +5,52 @@ public class TargetedAd {
 
   public static void main(String[] args)
   {
-    /*  
-     * TODO:
-     * PREPARATION WORK
-     * (1) Create a file called targetWords.txt. Populate this file with words on each line that
-     *     you think would determine if a user is a dog or cat owner.
-     * 
-     * PROGRAMMING
-     * (2) Create a new DataCollector object and set the data to "socialMediaPostsSmall.txt" and "targetWords.txt"
-     *     Important: Use the socialMedialPostsSmall to create your algorithm. Using a small file will help you 
-     *     generate your solution quicker and give you the ability to double check your work.
-     * (3) Create a String variable to hold the names of all the user. (The first word of every post is 
-     *     a person's username)
-     * (4) Compare each user's post to each target word. If a user mentions a target word, add their username to 
-     *     the String of users. Separate usernames with a space. 
-     *         Hint: You can use loops to look through each word. 
-     *         Hint2: You can use indexOf to check if a word is in a user post. 
-     * (5) Once you have all the users, use your DataCollector's prepareAdvertisement method to prepare a file 
-     *     with all users and the advertisement you will send them.
-     *         Additional Info: The prepareAdvertisement creates a new file on your computer. Check the posts of
-     *         some of the usernames to make sure your algorithm worked.
-     * 
-     * THE FINAL SOLUTION
-     * (6) Your solution should work with the socialMedialPostsSmall.txt. Modify your DataCollector initialization
-     *    so you use the socialMediaPosts.txt. You should now have a larger file of users to target.
-     */
+    DataCollector collector = new DataCollector();
+    collector.setData("socialMediaPosts.txt", "targetWords.txt");
 
-
-    /* your code here */
+    String targetedUsernames = "";
     
-     
-  }
+    String currentPost = collector.getNextPost();
 
+    while (!currentPost.equals("NONE"))
+    {
+      int firstSpace = currentPost.indexOf(" ");
+      String username = currentPost.substring(0, firstSpace);
+      String postContent = currentPost.substring(firstSpace + 1).toLowerCase();
+      
+      boolean isTargeted = false;
+      String specificPet = "pet"; 
+      
+      String targetWord = collector.getNextTargetWord();
+      while (!targetWord.equals("NONE"))
+      {
+        if (postContent.contains(targetWord.toLowerCase()))
+        {
+          isTargeted = true;
+          
+          if (targetWord.toLowerCase().contains("dog") || targetWord.toLowerCase().contains("pup")) {
+              specificPet = "dog";
+          } else if (targetWord.toLowerCase().contains("cat") || targetWord.toLowerCase().contains("kit")) {
+              specificPet = "cat";
+          }
+        }
+        targetWord = collector.getNextTargetWord();
+      }
+
+      if (isTargeted)
+      {
+        if (!targetedUsernames.contains(username))
+        {
+          targetedUsernames += username + " ";
+          
+          String advertisement = "Your furry friend will love our " + specificPet + " food!";
+          collector.prepareAdvertisement("targetedAds.txt", username, advertisement);
+        }
+      }
+      
+      currentPost = collector.getNextPost();
+    }
+    
+    System.out.println("Targeted advertisement file 'targetedAds.txt' has been generated.");
+  }
 }
